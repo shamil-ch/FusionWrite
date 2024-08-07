1ade058f1b9045431f2b0c2fcfc548a79d611e7e
shamil-ch <shamilkolekkara@gmail.com>
2024-08-07 11:57:25 +0530

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import './globals.css'
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata = {
+  title: 'FusionWrite',
+  description: 'your go-to collaborative editor',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
